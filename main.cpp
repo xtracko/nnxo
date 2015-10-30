885d9b1932dda2e7bdcb06696c3eb97be36cf794
Jiří Novotný <novotng@gmail.com>
2015-10-30 17:44:11 +0100

    Added simple test net

--- main.cpp
+++ main.cpp
@@ -1,16 +1,29 @@
 #include "net.hpp"
 #include <iostream>
 
+class sigmoid {
+public:
+    using type = float;
+public:
+    type operator()(type potencial) const
+    {
+        return 0;
+    }
+};
+
+using net_type = net<sigmoid, 3, 1>;
+
 int main()
 {
     std::cout << "Hello, I am a neural net!" << std::endl;
 
-    net<float, sigmoid, 3, 1> nn;
+    net<sigmoid, 3, 1> nn;
 
-    auto result = nn.run({0,0,0});
-    for (auto& r : result)
+    typename net_type::out_type out = {};
+    nn.run({0,0,0}, out);
+    for (auto& r : out)
         std::cout << r << " ";
-    std::cout << endl;
+    std::cout << std::endl;
 
     return 0;
 }
